485b162d00ca91f23c930edc4c4ec946d2f852b7
Daniel Cordova <dcordoba@udtonline.com>
2025-09-18 12:36:11 -0500

    add: include footer in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // here we can add footer and header
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>All rights reserved @ Daniel Cordova</footer>
+      </body>
     </html>
   );
 }
